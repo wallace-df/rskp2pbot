3efd529923adc8f67d730789ca0a7e2a1371a111
Wallace Ferreira <54377956+wallace-df@users.noreply.github.com>
2022-08-30 17:26:44 -0300

    Update db_connect.js

--- db_connect.js
+++ db_connect.js
@@ -5,9 +5,7 @@ const mongoose = require('mongoose');
 const credentials = process.env.DB_USER
   ? `${process.env.DB_USER}:${process.env.DB_PASS}@`
   : '';
-//const MONGO_URI = `mongodb://${credentials}${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?authSource=admin`;
-
-const MONGO_URI = `mongodb://${credentials}${process.env.DB_HOST}/?ssl=true&replicaSet=atlas-vv9t4r-shard-0&authSource=admin&retryWrites=true&w=majority`;
+const MONGO_URI = `mongodb://${credentials}${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?authSource=admin`;
 
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoDB URI');
